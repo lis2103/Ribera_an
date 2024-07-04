5c7ddd03e2406e29314caa42bd6dd8def238fa25
arondemeter1 <arondemeter@student.ie.edu>
2024-07-04 13:25:11 +0200

    backend

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__,
             template_folder='../frontend', 
-            static_folder='../backend/static')
+            static_folder='../frontend')
 
 # Landing page route
 @app.route('/')
