b0cf2af9630735e691c224d24e5dc28799c07509
mirandagcc <miranda.nichole.g@student.ie.edu>
2024-07-07 08:46:55 +0200

    Final Deployment

--- main.py
+++ main.py
@@ -10,13 +10,7 @@ def index():
 
 @app.route('/questions')
 def questions():
-    qa = [
-        {"question": "What social and health services will the elderly population require in the coming years, from 2025 to 2030?", "answer": "Answer 1"},
-        {"question": "Are there differences according to the regions of Spain and urban/rural areas?", "answer": "Answer 2"},
-        {"question": "And according to the profile of the elderly (archetypes)?", "answer": "Answer 3"},
-        {"question": "What role will digital technology play?", "answer": "Answer 4"},
-    ]
-    return render_template('questions.html', qa=qa)
+    return render_template('questions.html')
 
 @app.route('/graphs')
 def graphs():
@@ -33,15 +27,6 @@ def hospitalisation():
 @app.route('/graphs/other')
 def other():
     return render_template('other.html')
-
-@app.errorhandler(500)
-def internal_error(error):
-    return "500 error: " + str(error), 500
-
-@app.errorhandler(Exception)
-def unhandled_exception(e):
-    app.logger.error('Unhandled Exception: %s', (e))
-    return "Unhandled exception: " + str(e), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
